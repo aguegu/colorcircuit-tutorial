f1b0ca94a42448c066a05c1d2f6d4f6a93125985
aGuegu <weihong.guan@gmail.com>
2023-09-21 17:47:28 +0800

    init color space demo

---
+++ NeoGoggles/05-ColorSpace/01.c
@@ -0,0 +1,10 @@
+#include "neogoggles.h"
+
+void setup(void) {
+
+}
+
+void loop(void) {
+  setRGB(0, knobBrightness >> 4, knobColor >> 4, knobSpeed >> 4);
+  project(knobPattern >> 7, 1 << (knobSection / (4096 / 5)));
+}
