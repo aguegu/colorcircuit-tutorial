ae67c163a4b0207ad6beaa1ed116000bef4f22a4
aGuegu <weihong.guan@gmail.com>
2023-09-21 14:51:30 +0800

    tweak demo

--- NeoGoggles/03-Knobs/01.c
+++ NeoGoggles/03-Knobs/01.c
@@ -6,14 +6,15 @@ void setup(void) {
 
 void loop(void) {
   for (uint8_t i = 0; i < LED_LEN; i++) {
-    if (i >= knobSection / 128  && i < knobPattern / 128) {
-      frameBuffer[i + 0] = knobColor >> 4;
-      frameBuffer[i + 1] = knobBrightness >> 4;
-      frameBuffer[i + 2] = knobSpeed >> 4;
+    uint8_t j = i * 3;
+    if (i >= knobSection / 128  && i <= knobPattern / 128) {
+      frameBuffer[j + 0] = knobColor >> 4;
+      frameBuffer[j + 1] = knobBrightness >> 4;
+      frameBuffer[j + 2] = knobSpeed >> 4;
     } else {
-      frameBuffer[i + 0] = 0;
-      frameBuffer[i + 1] = 0;
-      frameBuffer[i + 2] = 0;
+      frameBuffer[j + 0] = 0;
+      frameBuffer[j + 1] = 0;
+      frameBuffer[j + 2] = 0;
     }
   }
 }
--- NeoGoggles/04-LayerOrigin/01.c
+++ NeoGoggles/04-LayerOrigin/01.c
@@ -6,7 +6,7 @@ void setup(void) {
 
 void loop(void) {
   origin[0] = knobColor >> 4;
-  origin[1] = knobBrightnessr >> 4;
+  origin[1] = knobBrightness >> 4;
   origin[2] = knobSpeed >> 4;
-  project(knobSection >> 7, 1 << (knobPattern / (4096 / 5)));
+  project(knobPattern >> 7, 1 << (knobSection / (4096 / 5)));
 }
