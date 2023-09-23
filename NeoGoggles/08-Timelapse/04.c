051ac940fee73ce4d86168b54353f2191cfe2de6
Weihong Guan <weihong.guan@cpnet.io>
2023-09-23 21:16:38 +0800

    timelaspse chapter

---
+++ NeoGoggles/08-Timelapse/04.c
@@ -0,0 +1,18 @@
+#include "neogoggles.h"
+
+void setup(void) {}
+
+void loop(void) {
+  static uint8_t step = 0;
+  uint8_t sections = 1 << (knobSection / (4095 / 5));
+
+  for (uint8_t i = 0; i < LED_LEN; i++) {
+    setHSB(i, (i + knobColor / (4096 / LED_LEN)) * 360 / LED_LEN , 255, knobBrightness >> 6);
+  }
+
+  project(step, sections);
+
+  if (getJiffies() % 10 == 0) {
+    step++;
+  }
+}
