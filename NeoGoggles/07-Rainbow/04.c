2555b4adb264539d638d117a6eadd203be4e139b
Weihong Guan <weihong.guan@cpnet.io>
2023-09-23 11:57:37 +0800

    tweak rainbow instruction

--- NeoGoggles/07-Rainbow/04.c
+++ NeoGoggles/07-Rainbow/04.c
@@ -8,7 +8,7 @@ void loop(void) {
   uint8_t sections = 1 << (knobSection / (4095 / 5));
 
   for (uint8_t i = 0; i < LED_LEN; i++) {
-    setHSB(i, (i + knobColor * LED_LEN / 4096) * 360 / LED_LEN , 255, knobBrightness >> 6);
+    setHSB(i, (i + knobColor / 128) * 360 / LED_LEN , 255, knobBrightness >> 6);
   }
 
   project(knobPattern >> 7, sections);
